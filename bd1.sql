04dacb713bbfa01864fa8f2d585972d1035c6568
Pool puma <202000188g@utea.edu.pe>
2024-02-09 08:37:23 -0500

    se agrega el proyecto

---
+++ bd1.sql
@@ -0,0 +1,25 @@
+-- Tabla de Estudiantes
+CREATE TABLE Estudiantes (
+    EstudianteID INT PRIMARY KEY,
+    Nombre VARCHAR(50),
+    Apellido VARCHAR(50),
+    -- Otros campos relacionados con el estudiante
+);
+
+-- Tabla de Cursos
+CREATE TABLE Cursos (
+    CursoID INT PRIMARY KEY,
+    NombreCurso VARCHAR(50),
+    -- Otros campos relacionados con el curso
+);
+
+-- Tabla de Pagos
+CREATE TABLE Pagos (
+    PagoID INT PRIMARY KEY,
+    EstudianteID INT,
+    CursoID INT,
+    MontoPagado DECIMAL(10, 2),
+    FechaPago DATE,
+    FOREIGN KEY (EstudianteID) REFERENCES Estudiantes(EstudianteID),
+    FOREIGN KEY (CursoID) REFERENCES Cursos(CursoID)
+);
